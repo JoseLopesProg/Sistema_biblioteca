85dbd6fb505ed62634ba65d1a5b83e0aed0e7572
Jose Lopes <neto.lpc@gmail.com>
2026-07-12 21:22:50 -0300

    Sistema v1.0
    Todo o programa já pronto pra beta teste.

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from time import sleep
 import os
 from funções import *
 import sqlite3
+from dados import *
 
 conexão = sqlite3.connect('banco.db')
 cursor = conexão.cursor()
 
 #Criação da Tabela
-cursor.execute("""CREATE TABLE IF NOT EXISTS contas_bancárias (
+cursor.execute("""CREATE TABLE IF NOT EXISTS livros (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     Titulo TEXT NOT NULL,
     Autor TEXT NOT NULL,
@@ -15,34 +16,37 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS contas_bancárias (
     Quantidade INTEGER
     )""")
 
-#Solicitar dados ao usuário
-cabeçalho('SISTEMA BIBLIOTECA')
+while True:
+    cabeçalho('SISTEMA BIBLIOTECA')
+    #SOLICITANDO OS DADOS AO USUÁRIO
+    resp=menu(['Cadastrar livros','Excluir livros', 'Editar livros','Pesquisar','Ver lista de livros', 'Sair do sistema'])
+    match resp:
+        case 1:
+            #CADASTRO DOS LIVROS
+            cadastrar()
+            os.system('cls')
+        case 2:
+            #DELETAR LIVROS CADASTRADOS
+            excluir()
+            os.system('cls')
+        case 3:
+            #ATUALIZANDO LIVROS CADASTRADOS
+            editar()
+            os.system('cls')
+        case 4:
+            #PESQUISA DE LIVROS CADASTRADOS
+            pesquisa()
+            os.system('cls')
+        case 5:
+            #LISTAR TODOS OS LIVROS CADASTRADOS
+            cabeçalho('Ver lista de livros')
+            lista = listar_livros()
+            for livro in lista:
+                print(f'ID: {livro[0]} | Titulo: {livro[1]} | Autor: {livro[2]} | Editora: {livro[3]} | Quantidade: {livro[4]}')
+            input('\nPressione ENTER para voltar ao menu...')
+            os.system('cls')
+        case _:
+            cabeçalho('Saindo do sistema... Até logo!')
+            break
 
-resp=menu(['Cadastrar livros','Excluir livros', 'Editar livros','Pesquisar','Ver lista de livros', 'Sair do sistema'])
-
-match resp:
-    case 1:
-        cabeçalho('Cadastrar livros')
-        sleep(2)
-        os.system('cls')
-    case 2:
-        cabeçalho('Excluir livros')
-        sleep(2)
-        os.system('cls')
-    case 3:
-        cabeçalho('Editar Livros')
-        sleep(2)
-        os.system('cls')
-    case 4:
-        cabeçalho('Pesquisar')
-        sleep(2)
-        os.system('cls')
-    case 5:
-        cabeçalho('Ver lista de livros')
-        sleep(2)
-        os.system('cls')
-    case _:
-        cabeçalho('Saindo do sistema... Até logo!')
-
-
-conexão.commit()
+print('Obrigado por ultilizar o programa!')
